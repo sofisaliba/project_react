dfeaacc0c48677327e2d243dfd17e4dd94995270
Sofiia <sofiia@MacBook-Air-Sofiia.local>
2024-04-27 13:06:22 +0200

    fix link

--- src/components/Card/RouteForCards/content.jsx
+++ src/components/Card/RouteForCards/content.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+
 
 import styles from './content.module.css'
 
 
 function Content() {
     return (
-      // <Router>
+      
         <div className={styles.main}>
     <p className={styles.name}>Учи язык эффективно</p>
 
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,6 +1,6 @@
 //import { Card } from 'react-bootstrap';
 import styles from './footer.module.css'
-import { BrowserRouter as Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
