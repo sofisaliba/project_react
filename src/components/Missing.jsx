f55ee49fcce3821c79e3663b82b6331601ad979b
Sofiia <sofiia@MacBook-Air-Sofiia.local>
2024-05-02 10:39:37 +0200

    add gh-pages

--- src/components/Missing.jsx
+++
@@ -1,9 +0,0 @@
-function Missing () {
-    return(
-    <main>
-        <h1>Error 404</h1>
-    </main>
-    );
-}
-
-export default Missing;
--- src/components/buttonOnCard.jsx
+++
@@ -1,7 +0,0 @@
-
-
-function AddButton() {
-    return <button className="card-add">Проверить</button>;
-}
-
-export default AddButton
